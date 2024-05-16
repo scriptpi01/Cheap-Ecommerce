e903dd4bf3bc270bc7c78e3d0ec63924f1651910
scriptpi01 <manopakorn.kooh@bumail.net>
2024-05-17 06:39:53 +0700

    Update project files and redeploy

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const Home1 = lazy(() => import('./pages/Home1'));
 
 const App = () => {
   return (
-    <Router basename="/Cheap-Ecommerce"> // Ensure this matches your repo name
+    <Router basename="/Cheap-Ecommerce"> 
       <AuthProvider>
         <Navbar />
         <Chat />
