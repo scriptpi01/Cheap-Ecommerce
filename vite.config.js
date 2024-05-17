b0a35e1f8bd75e411380f59a6289fdef8acbae32
scriptpi01 <manopakorn.kooh@bumail.net>
2024-05-17 14:36:13 +0700

    Update project files and redeploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Cheap-Ecommerce/', 
-  plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000,
-  },
-});
+  plugins: [react({
+    // Add this line
+    include: "**/*.jsx",
+  })]
+})
