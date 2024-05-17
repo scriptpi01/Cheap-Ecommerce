c408a90333518e9c3cc016c23b9e17d690b56b50
scriptpi01 <manopakorn.kooh@bumail.net>
2024-05-17 14:44:40 +0700

    Update project files and redeploy

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,6 @@ export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        404: resolve(__dirname, "public/404.html"),
-      },
-    },
+    
   },
 });
