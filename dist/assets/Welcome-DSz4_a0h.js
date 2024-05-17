50bcff1f1b1a01fc78c767a76ebf0f760f143ecb
scriptpi01 <manopakorn.kooh@bumail.net>
2024-05-17 14:55:17 +0700

    Update project files and redeploy

--- dist/assets/Welcome-DSz4_a0h.js
+++
@@ -1,1 +0,0 @@
-import{f as s,j as t}from"./index-B3Gr2Rn1.js";const o=()=>{const e=s(),a=()=>{e("/home")};return t.jsxs("div",{className:"welcome-container",children:[t.jsx("div",{className:"icon-3d",children:"🛒"}),"  ",t.jsx("button",{onClick:a,className:"start-button",children:"Start Shopping Cheap"})]})};export{o as default};
